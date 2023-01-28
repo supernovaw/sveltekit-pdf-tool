f0e3833112c4fedfedda59f28c835f2078ca42fc
supernovaw <ruszrj@gmail.com>
2023-01-28 07:07:40 -0800

    add layout & tweak css

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { base } from "$app/paths";
+</script>
+
+<nav>
+  <ul>
+    <li><a href="{base}">Home</a></li>
+  </ul>
+</nav>
+<main>
+  <slot />
+</main>
+
+<style>
+  nav {
+    display: flex;
+    justify-content: flex-end;
+    overflow: hidden;
+  }
+
+  a {
+    padding-inline: 16px;
+    margin-inline: 0;
+  }
+
+  main {
+    padding: 32px;
+    text-align: center;
+  }
+</style>
